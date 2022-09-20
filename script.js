55deaf53bb1e92fd7bf65b34b6ac52a6301c4ad9
limorkeez <l.morkaitis@gmail.com>
2022-09-20 07:52:33 +0300

    Added seventy-five percent

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const calculateBtn = document.querySelector('#calculateBtn');
 let summaryAmount = document.querySelector('#summaryAmount');
 let summaryOne = document.querySelector('#summaryOne');
 let summaryTwo = document.querySelector('#summaryTwo');
+let summaryThree = document.querySelector('#summaryThree');
 
 
 calculateBtn.addEventListener('click', () => {
@@ -16,6 +17,7 @@ calculateBtn.addEventListener('click', () => {
     const goalValue = parseInt(monthlyGoal.value);
     const currentValue = parseInt(monthlyCurrent.value);
     const seventy = goalValue*0.7;
+    const seventyFive = goalValue*0.75;
     const eighty = goalValue*0.8;
 
     const currentPercentage = (currentValue*100)/goalValue;
@@ -23,7 +25,8 @@ calculateBtn.addEventListener('click', () => {
     if(goalValue && currentValue){
         summaryAmount.innerHTML = currentPercentage > 100 ? '100%' : currentPercentage.toFixed(2)+'%';
         summaryOne.innerHTML = Math.ceil(seventy-currentValue) > 0 ? Math.ceil(seventy-currentValue)+'€' : 0+'€';
-        summaryTwo.innerHTML = Math.ceil(eighty-currentValue) > 0 ? Math.ceil(eighty-currentValue)+'€' : 0+'€';
+        summaryTwo.innerHTML = Math.ceil(seventyFive-currentValue) > 0 ? Math.ceil(seventyFive-currentValue)+'€' : 0+'€';
+        summaryThree.innerHTML = Math.ceil(eighty-currentValue) > 0 ? Math.ceil(eighty-currentValue)+'€' : 0+'€';
     }
     else{
         alert('Įvesk mėnesio planą ir esamą surinkimą. Galimi tik skaičiai');
@@ -35,6 +38,7 @@ resetBtn.addEventListener('click', () => {
     summaryAmount.innerHTML = 0;
     summaryOne.innerHTML = 0;
     summaryTwo.innerHTML = 0;
+    summaryThree.innerHTML = 0;
     monthlyGoal = '';
     monthlyCurrent = '';
 })
